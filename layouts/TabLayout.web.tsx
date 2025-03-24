2ffe4fed47c77ab724db0f50289ad02488afc8a2
Douglas Lowder <douglowder@mac.com>
2025-03-23 20:57:03 -0700

    Router UI and nativewind styles for tab bar on web

--- layouts/TabLayout.web.tsx
+++ layouts/TabLayout.web.tsx
@@ -1,40 +1,54 @@
-import { Colors } from '@/constants/Colors';
-import { Tabs } from 'expo-router';
+import {
+  Tabs,
+  TabSlot,
+  TabList,
+  TabTrigger,
+  TabTriggerSlotProps,
+} from 'expo-router/ui';
 import React from 'react';
+import { Pressable, Text } from 'react-native';
+
+import '../global.css';
+import { vars } from 'nativewind';
+import { nativewindColorTheme } from '@/constants/Colors';
+
+const theme = vars(nativewindColorTheme);
+
+function CustomTabButton(props: TabTriggerSlotProps) {
+  return (
+    <Pressable className="flex-1 justify-center items-center" {...props}>
+      <Text
+        className={
+          props.isFocused
+            ? 'text-[5vh] active:text-blue-500'
+            : 'text-[3vh] active:text-blue-500'
+        }
+      >
+        {props.children}
+      </Text>
+    </Pressable>
+  );
+}
+CustomTabButton.displayName = 'CustomTabButton';
 
 export default function TabLayout() {
-  const tabBarActiveTintColor = Colors.light.tint;
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor,
-        tabBarPosition: 'top',
-        tabBarIcon: () => null,
-        tabBarLabelStyle: {
-          fontSize: 30,
-          lineHeight: 60,
-        },
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Two',
-        }}
-      />
-      <Tabs.Screen
-        name="tvdemo"
-        options={{
-          title: 'TV styling demo',
-        }}
-      />
+    <Tabs>
+      <TabList
+        style={theme}
+        className="flex flex-row absolute top-0 width-full h-[10vh] bg-[--background-light] dark:bg-[--background-dark]"
+      >
+        <TabTrigger name="index" href="/" asChild>
+          <CustomTabButton>Home</CustomTabButton>
+        </TabTrigger>
+        <TabTrigger name="two" href="/(tabs)/two" asChild>
+          <CustomTabButton>Two</CustomTabButton>
+        </TabTrigger>
+        <TabTrigger name="tvdemo" href="/(tabs)/tvdemo" asChild>
+          <CustomTabButton>TV demo</CustomTabButton>
+        </TabTrigger>
+      </TabList>
+      <TabSlot />
     </Tabs>
   );
 }
